98f5312eb1cde9a2a4f705ff8bab2aa386cad9c1
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-16 11:42:39 +0400

    new proekt 9

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            user users = new user();
+            Console.WriteLine(" enter the username :");
+            users.Username = Console.ReadLine();
+            Console.WriteLine("enter the password :");
+            users.Password = Console.ReadLine();
+            Console.WriteLine(users.Username);
+            Console.WriteLine(users.Password);
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApp2/user.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class user
+    {
+        private string _Username;
+        private string _Password;
+        public string Username
+        {
+            get
+            {
+                return this._Username;
+            }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25)
+                {
+                    this._Username = value;
+                }
+            }
+        }
+        public string Password
+        {
+
+            get { return this._Password; }
+            set
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (value.Length >= 8 && value.Length <= 25 && HasLower(value) && HasUpper(value) && HasDigit(value)) 
+                    {
+                        this.Password = value;
+                    }
+                }
+
+            }
+        }
+        static bool HasDigit(string txt)
+        {
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (Char.IsDigit(txt[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static bool HasUpper(string txt)
+        {
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (Char.IsUpper(txt[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static bool HasLower(string txt)
+        {
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (Char.IsLower(txt[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }   
+}
